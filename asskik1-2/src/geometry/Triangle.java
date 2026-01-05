5c211518b520b75f96f1a7da7b80e4e83fd1f80d
Aluana <Muslimova>
2026-01-06 02:15:20 +0500

    4  commit

--- asskik1-2/src/geometry/Triangle.java
+++ asskik1-2/src/geometry/Triangle.java
@@ -8,6 +8,28 @@ public class Triangle
 
     public Triangle ()
     {
+        this.p1 = new Point(0,0);
+        this.p2 = new Point(1,0);
+        this.p3 = new Point(0,1);
+    }
 
+    public Triangle(Point p1, Point p2, Point p3)
+    {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+    }
+
+    public double getPerimeter()
+    {
+        return p1.distance(p2) + p2.distance(p3) + p3.distance(p1);
+    }
+
+    public double getArea()
+    {
+        return Math.abs(
+                p1.getX() * (p2.getY() - p3.getY())+
+                p2.getX() * (p3.getY() - p1.getY())+
+                p3.getX() * (p1.getY() - p2.getY())) / 2;
     }
 }
