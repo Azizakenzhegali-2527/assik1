788fd8821663c4efb00e4e9311f9f26306715b10
Aluana <Muslimova>
2026-01-06 08:15:19 +0500

    8   commit

--- asskik1-2/src/geometry/Circle.java
+++ asskik1-2/src/geometry/Circle.java
@@ -2,19 +2,64 @@ package geometry;
 
 public class Circle
 {
-    private Point center;
+    private double x;
+    private double y;
     private double radius;
 
     public Circle()
     {
-        this.center = new Point(0,0);
-        this.radius = 1;
+        x=0;
+        y=0;
+        radius = 1;
     }
 
-    public Circle(Point center, double radius)
+    public Circle(double x, double y , double radius)
     {
-        this.center = center;
+        this.x = x;
+        this.y = y;
         this.radius = radius;
+    }
+
+    public double getX()
+    {
+        return x;
+    }
+
+
+    public double getY()
+    {
+        return y;
+    }
+
+    public double getRadius()
+    {
+        return radius;
+    }
+
+    public boolean contains(double x, double y )
+    {
+        double dx = x - this.x;
+        double dy = y- this.y;
+        double distance = Math.sqrt(dx*dx + dy*dy);
+        return distance <= radius;
+    }
+
+
+    public boolean contains(Circle circle)
+    {
+        double dx = circle.x - this.x;
+        double dy = circle.y - this.y;
+        double distance = Math.sqrt(dx*dx - dy*dy);
+        return distance + circle.radius <= radius;
+    }
+
+    public boolean overlaps(Circle circle)
+    {
+        double dx = circle.x - this.x;
+        double dy = circle.y - this.y;
+        double distance = Math.sqrt( dx*dx - dy*dy);
+        return distance < radius + circle.radius;
+
     }
 
     public double getArea()
@@ -26,4 +71,5 @@ public class Circle
     {
         return 2 * Math.PI * radius;
     }
+
 }
