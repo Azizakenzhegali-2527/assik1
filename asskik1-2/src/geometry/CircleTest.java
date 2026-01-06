3e4a6726dd9a679d38d6edd0ec6b2fe7fd568f98
Aluana <Muslimova>
2026-01-06 08:19:57 +0500

    9   commit

--- asskik1-2/src/geometry/CircleTest.java
+++ asskik1-2/src/geometry/CircleTest.java
@@ -4,8 +4,14 @@ public class CircleTest
 {
     public static void main(String[] args)
     {
-        Circle c = new Circle( new Point(0,0), 5);
-        System.out.println(c.getArea());
-        System.out.println(c.getPerimeter());
+        Circle c1 = new Circle( 0, 0,5);
+        Circle c2 = new Circle(2,2,2);
+        Circle c3 = new Circle(10,10,1);
+        System.out.println(c1.contains(1,1));
+        System.out.println(c1.contains(c2));
+        System.out.println(c1.contains(c3));
+
+        System.out.println(c1.overlaps(c2));
+        System.out.println(c1.overlaps(c3));
     }
 }
